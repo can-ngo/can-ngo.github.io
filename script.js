1658f58492c08ef8f77be5c5b7da776244ba351a
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-05-20 07:04:41 +0700

    Code the Javascript, add variables

---
+++ script.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById("matrix");
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+
+const katakana = "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン";
+const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const nums = "0123456789";
+
+const alphabet = katakana + latin + nums;
+
+const fontSize = 14;
+const columns = canvas.clientWidth/fontSize;
+
+const rainDrops = [];
