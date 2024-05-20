7f13815cc6d742eecb5bec2f03fdb570bd634e0c
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-05-20 19:18:44 +0700

    Draw canvas

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ const fontSize = 14;
 const columns = canvas.clientWidth/fontSize;
 
 const rainDrops = [];
+
+for(let x = 0; x < columns; x++) {
+    rainDrops[x] = 1;
+}
+
+const draw = () => {
+    context.fillStyle = 'rgba(255,255,255,0.25)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = '#000000';
+    context.font = fontSize + 'px monospace';
+
+    for (let i = 0; i < rainDrops.length; i++)
+        {
+            const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+            context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+
+            if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.950) {
+                rainDrops[i]=0;
+            }
+            rainDrops[i]++;
+        }
+};
+
+setInterval(draw, 90);
