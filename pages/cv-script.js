4efba53f5baf13f2abcc34480ccee096c8005749
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-05-28 19:17:52 +0700

    Add darkmode styling and update cv-script

--- pages/cv-script.js
+++ pages/cv-script.js
@@ -1,1 +1,12 @@
 // toggle dark-mode
+let dark = document.getElementById("icon-dark-mode");
+
+dark.addEventListener('click',function(){
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+    console.log(dark.classList);
+});
+
+
+   
+   
