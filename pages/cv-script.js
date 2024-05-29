ec88d2117ed26aa2e942767b1a019045c4d57f84
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-05-29 13:52:01 +0700

    Update cv-script to toggle color of home icon

--- pages/cv-script.js
+++ pages/cv-script.js
@@ -7,14 +7,18 @@ dark.addEventListener('click',function(){
 
     const nav = document.querySelector("nav");
     nav.classList.toggle("dark-mode");
+
+    const icon = document.getElementById("icon-back");
     
     // toggle icon sun-moon
     switch(dark.classList[1]){
         case "fa-moon":
             dark.classList.replace("fa-moon","fa-sun");
+            icon.style.color = "white";
             break;
         case "fa-sun":
             dark.classList.replace("fa-sun","fa-moon");
+            icon.style.color = "black";
             break;
     }    
 });
