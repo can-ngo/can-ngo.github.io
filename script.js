c62ccec5d874010b6f2f01f381209c6020220d6c
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-06-02 20:09:33 +0700

    Modified script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//Declare icon light variable
+let light = document.getElementById("icon-light-mode");
+
+const body = document.body;
+const nav = document.querySelector("nav");
+
 //Draw matrix canvas
 const canvas = document.getElementById("matrix");
 const context = canvas.getContext('2d');
@@ -21,30 +27,10 @@ for(let x = 0; x < columns; x++) {
 }
 
 const draw = () => {
-    context.fillStyle = 'rgba(255,255,255,0.25)';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    context.fillStyle = '#000000';
-    context.font = fontSize + 'px monospace';
-
-    for (let i = 0; i < rainDrops.length; i++)
-        {
-            const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-            context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
-
-            if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.950) {
-                rainDrops[i]=0;
-            }
-            rainDrops[i]++;
-        }
-};
-// setInterval(draw, 80);
-
-const drawDark = () => {
     context.fillStyle = 'rgba(0,0,0,0.25)';
     context.fillRect(0, 0, canvas.width, canvas.height);
-
     context.fillStyle = '#FFFFFF';
+    
     context.font = fontSize + 'px monospace';
 
     for (let i = 0; i < rainDrops.length; i++)
@@ -57,17 +43,33 @@ const drawDark = () => {
             }
             rainDrops[i]++;
         }
-};
+}
+setInterval(draw,80);
+
+// const draw = () => {
+//     context.fillStyle = 'rgba(255,255,255,0.25)';
+//     context.fillRect(0, 0, canvas.width, canvas.height);
+
+//     context.fillStyle = '#000000';
+//     context.font = fontSize + 'px monospace';
+
+//     for (let i = 0; i < rainDrops.length; i++)
+//         {
+//             const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+//             context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+
+//             if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.950) {
+//                 rainDrops[i]=0;
+//             }
+//             rainDrops[i]++;
+//         }
+// };
 
 // toggle light-mode
-let light = document.getElementById("icon-light-mode");
-
 light.addEventListener('click',function(){
-    const body = document.body;
     body.classList.toggle("light-mode");
-    const nav = document.querySelector("nav");
     nav.classList.toggle("light-mode");
-    
+
     // toggle icon sun-moon
     switch(light.classList[1]){
         case "fa-moon":
@@ -76,18 +78,11 @@ light.addEventListener('click',function(){
             break;
         case "fa-sun":
             light.classList.replace("fa-sun","fa-moon");
-            
+                       
             break;
     }    
 });
 
-if (!document.body.classList.contains("light-mode"))
-    {
-        let matrixRunDark = setInterval(drawDark,80);
-    
-    } else {
-        let matrixRunLight = setInterval(draw,80);
-        
-    }
+
 
 
