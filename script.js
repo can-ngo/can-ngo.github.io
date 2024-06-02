f1cc444a54533c026b9906bfa40aa28fa2b777e2
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-06-02 10:49:11 +0700

    Modified draw & drawDark function
    Add Hiragana into Matrix.
    Change default body background to dark

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const context = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 
-const katakana = "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン";
+const katakana = "アカサタナハマヤャラワガザダバパイキシチニヒミリヰギジヂビピウクスツヌフムユルグズブヅプエケセテネヘメレヱゲゼデベペオコソトノホモヨロヲゴゾドボポヴッン";
+const hiragana ="あいうえおかきくけこさしすせそなにぬねのはひふへほん"; //not enough
 const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const nums = "0123456789";
 
-const alphabet = katakana + latin + nums;
+const alphabet = katakana + latin + hiragana + nums;
 
-const fontSize = 14;
+const fontSize = 12;
 const columns = canvas.clientWidth/fontSize;
 
 const rainDrops = [];
@@ -37,17 +38,35 @@ const draw = () => {
             rainDrops[i]++;
         }
 };
+// setInterval(draw, 80);
 
-setInterval(draw, 80);
+const drawDark = () => {
+    context.fillStyle = 'rgba(0,0,0,0.25)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
-// toggle dark-mode
-let dark = document.getElementById("icon-dark-mode");
+    context.fillStyle = '#FFFFFF';
+    context.font = fontSize + 'px monospace';
+
+    for (let i = 0; i < rainDrops.length; i++)
+        {
+            const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+            context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+
+            if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.950) {
+                rainDrops[i]=0;
+            }
+            rainDrops[i]++;
+        }
+};
+
+// toggle light-mode
+let dark = document.getElementById("icon-light-mode");
 
 dark.addEventListener('click',function(){
     const body = document.body;
-    body.classList.toggle("dark-mode");
+    body.classList.toggle("light-mode");
     const nav = document.querySelector("nav");
-    nav.classList.toggle("dark-mode");
+    nav.classList.toggle("light-mode");
     
     // toggle icon sun-moon
     switch(dark.classList[1]){
@@ -59,3 +78,5 @@ dark.addEventListener('click',function(){
             break;
     }    
 });
+
+setInterval(drawDark,80);
